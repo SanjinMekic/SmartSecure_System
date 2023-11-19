da8377444416b1ee5bfecaaaadaed0795f95ed53
Sanjin Mekić <83376894+Sanjin22@users.noreply.github.com>
2023-11-19 23:06:27 +0100

    Update IoTseminar_finalProject.ino

--- IoTseminar_finalProject.ino
+++ IoTseminar_finalProject.ino
@@ -1,14 +1,3 @@
-/*
-  Rui Santos
-  Complete project details at our blog.
-    - ESP32: https://RandomNerdTutorials.com/esp32-firebase-realtime-database/
-    - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-firebase-realtime-database/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
-  https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
-*/
-
 #include <Arduino.h>
 #if defined(ESP32)
   #include <WiFi.h>
@@ -17,22 +6,16 @@
 #endif
 #include <Firebase_ESP_Client.h>
 
-//Provide the token generation process info.
 #include "addons/TokenHelper.h"
-//Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 
-// Insert your network credentials
 #define WIFI_SSID "YOUR_WIFI"
 #define WIFI_PASSWORD "YOUR_PASSWORD"
 
-// Insert Firebase project API Key
 #define API_KEY "YOUR_API_KEY"
 
-// Insert RTDB URLefine the RTDB URL */
 #define DATABASE_URL "YOUR_DATABASE_URL" 
 
-//Define Firebase Data object
 FirebaseData fbdo;
 
 FirebaseAuth auth;
@@ -65,13 +48,10 @@ void setup(){
   Serial.println(WiFi.localIP());
   Serial.println();
 
-  /* Assign the api key (required) */
   config.api_key = API_KEY;
 
-  /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
 
-  /* Sign up */
   if (Firebase.signUp(&config, &auth, "", "")){
     Serial.println("ok");
     signupOK = true;
@@ -80,8 +60,7 @@ void setup(){
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
   }
 
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  config.token_status_callback = tokenStatusCallback; 
   
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
